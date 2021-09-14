69a88625fe3db57f979c7d644f49bac6ea4a2d7b
alan <alan.ghb@gmail.com>
2021-09-13 21:52:56 -0300

    carga total

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router.get("/pessoas", PessoaController.pegaPessoasAtivas);
+router.get("/pessoas/todos", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+//
+router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
+//
+router.get("/pessoas/:estudanteId/matricula/:matriculaId", PessoaController.pegaUmaMatricula);
+router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
+router.put("/pessoas/:estudanteId/matricula/:matriculaId", PessoaController.atualizaMatricula);
+router.delete("/pessoas/:estudanteId/matricula/:matriculaId", PessoaController.apagaMatricula);
+//
+router.get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas);
+router.get("/pessoas/matricula/:turmaId/confirmadas", PessoaController.pegaMatriculasPorTurma);
+router.get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas);
+router.post("/pessoas/:estudanteId/cancela", PessoaController.cancelaPessoa);
+
+module.exports = router;
